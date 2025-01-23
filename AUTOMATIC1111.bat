d6b6f564346ebb4a6618f47e99a47c69729cb763
Jorden Tse <jordenyt@gmail.com>
2025-01-23 11:16:12 +0800

    update

--- AUTOMATIC1111.bat
+++ AUTOMATIC1111.bat
@@ -1,5 +1,5 @@
 cd /d E:\Workspace\stable-diffusion-webui
 rem git fetch origin
 rem git merge origin/master
-rem call C:\Users\Jorden\anaconda3\condabin\activate a1111
+rem call C:\Users\Jorden\anaconda3\condabin\activate base
 call webui-user.bat
